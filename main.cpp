d9dda39f7a09cac281576211b767e69373a1b66a
Gleb <liftchampion@gmail.com>
2020-06-19 21:01:55 +0300

    ++

--- main.cpp
+++ main.cpp
@@ -86,9 +86,6 @@ class TcpDirect_and_EfVi {
             std::cout << "zf_stack_alloc err" << std::endl;
             return false;
         }
-//        if (zf_muxer_alloc(stack, &muxer)) {
-//            std::cout << "zf_muxer_alloc err" << std::endl;
-//        }
         return init_ev_fi(interface_name);
     }
 
@@ -105,7 +102,8 @@ class TcpDirect_and_EfVi {
             return false;
         }
         int flags = EF_VI_FLAGS_DEFAULT;
-        if (ef_vi_alloc_from_pd(&vi, driver_handle, &pd, driver_handle, -1, 0, -1, nullptr, -1, static_cast<enum ef_vi_flags >(flags)) < 0) {
+        if (ef_vi_alloc_from_pd(&vi, driver_handle, &pd, driver_handle,
+                -1, 0, -1, nullptr, -1, static_cast<enum ef_vi_flags >(flags)) < 0) {
             std::cout << "ef_vi_alloc_from_pd err" << std::endl;
             return false;
         }
@@ -147,7 +145,6 @@ class TcpDirect_and_EfVi {
     inline void deinit() noexcept
     {
         deinit_ev_fi();
-//        zf_muxer_free(muxer);
         if (zf_stack_free(stack)) {
             std::cout << "zf_stack_free err" << std::endl;
         }
@@ -177,9 +174,8 @@ class TcpDirect_and_EfVi {
     }
 
 
-    zf_attr*         attr = nullptr;
+    zf_attr*         attr  = nullptr;
     zf_stack*        stack = nullptr;
-//    zf_muxer_set*    _muxer = nullptr;
     ef_pd            pd = {};
     ef_pio           pio = {};
     ef_driver_handle driver_handle = {};
@@ -193,7 +189,11 @@ class TcpDirect_and_EfVi {
 class Zocket {
   public:
 
-    Zocket(TcpDirect_and_EfVi* tcp_direct_and_ef_vi) : tcp_direct_and_ef_vi(tcp_direct_and_ef_vi) {}
+    Zocket(TcpDirect_and_EfVi* tcp_direct_and_ef_vi, int msg_declared_len, int msg_actual_len)
+      : tcp_direct_and_ef_vi(tcp_direct_and_ef_vi)
+      , msg_declared_len(msg_declared_len)
+      , msg_actual_len(msg_actual_len)
+    {}
 
     inline bool open(const std::string &ip_port) noexcept
     {
@@ -221,6 +221,84 @@ class Zocket {
         return true;
     }
 
+    inline void do_write() noexcept
+    {
+        fill_msg_with_rand();
+#ifdef USE_COPY_PIO
+        create_header(0, false);
+        prepare_data_for_copy_pio();
+        ef_vi_transmit_pio_warm(&tcp_direct_and_ef_vi->vi);
+        write_with_copy_pio();
+#else
+        create_header(0, true);
+        ef_vi_transmit_pio_warm(&tcp_direct_and_ef_vi->vi);
+        write();
+#endif
+        tcp_direct_and_ef_vi->pio_in_use = true;
+        complete_send();
+    }
+
+
+    inline void fill_msg_with_rand() noexcept
+    {
+        for (size_t i = 0; i < msg_actual_len; ++i) { send_buff[i] = rand() % 90 + 33; }
+    }
+
+    inline bool create_header(uint64_t timestamp, bool put_to_pio) noexcept
+    {
+        _zfds.headers_size = MAX_ETH_HEADERS + MAX_IP_TCP_HEADERS;
+        _zfds.headers = headers_buf;
+        if (zf_delegated_send_prepare(_socket, _max_send_size, 40960, 0, &(_zfds)) != ZF_DELEGATED_SEND_RC_OK) {
+            std::cout << "zf_delegated_send_prepare err" << std::endl;
+            return false;
+        }
+        auto tcp_flags_p = reinterpret_cast<uint8_t*>((size_t)_zfds.headers + _zfds.tcp_seq_offset + _tcp_offset_seq_to_flags);
+        if (_push) { *tcp_flags_p |= _tcp_flag_psh; }
+        else { *tcp_flags_p &= ~_tcp_flag_psh; }
+        if (put_to_pio) {
+            if (ef_pio_memcpy(&tcp_direct_and_ef_vi->vi, _zfds.headers, 0, _zfds.headers_size)) {
+                std::cout << "ef_pio_memcpy err" << std::endl;
+                return false;
+            }
+//            _header_timestamp = timestamp;
+            if (!set_message_size(314)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    inline bool set_message_size(uint16_t size) noexcept
+    {
+        uint16_t size_to_set = htons(size + _zfds.ip_tcp_hdr_len);
+        if (ef_pio_memcpy(&tcp_direct_and_ef_vi->vi, &size_to_set, _zfds.ip_len_offset, sizeof(size_to_set))) {
+            std::cout << "ef_pio_memcpy err" << std::endl;
+            return false;
+        }
+        return true;
+    }
+
+    inline void prepare_data_for_copy_pio()
+    {
+        memcpy(headers_buf + _zfds.headers_len, send_buff, msg_actual_len);
+        zf_delegated_send_tcp_update(&_zfds, msg_actual_len, _push);
+        pio_data_len = _zfds.headers_len + msg_actual_len;
+    }
+
+    inline void write_with_copy_pio() noexcept {
+        ef_vi_transmit_copy_pio(&tcp_direct_and_ef_vi->vi, pio_offset, headers_buf, pio_data_len, pio_id);
+    }
+
+    inline void complete_send() noexcept
+    {
+        struct iovec iov;
+        iov.iov_base = send_buff;
+        iov.iov_len = msg_actual_len;
+        if (zf_delegated_send_complete(_socket, &iov, 1, 0) < 0) {
+            std::cout << "zf_delegated_send_complete err" << std::endl;
+        }
+    }
+
 
     inline bool close() noexcept
     {
@@ -242,7 +320,8 @@ class Zocket {
 
     TcpDirect_and_EfVi* tcp_direct_and_ef_vi = {};
 
-
+    int msg_declared_len = 80;
+    int msg_actual_len = 80;
 
     static constexpr uint64_t _max_send_size = 400;
     static constexpr uint32_t _tcp_flag_psh = 0x8;
@@ -251,18 +330,19 @@ class Zocket {
 
     zft_handle* _socket_handle = nullptr;
     zft*        _socket = nullptr;
+    zf_ds       _zfds = {};
 
-    uint64_t _pio_offset = 0;
-    uint64_t _pio_data_len = 0;
-    uint64_t _max_iov = 1;
-    uint64_t _header_timestamp = 0;
+    uint64_t pio_offset = 0;
+    uint64_t pio_data_len = 0;
+    uint64_t max_iov = 1;
+    uint64_t header_timestamp = 0;
 
-    int _pio_id = 0;
+    int pio_id = 0;
 
 
-    char _headers_buf[MAX_ETH_HEADERS + MAX_IP_TCP_HEADERS + _max_send_size];
-    char _send_buff[1024] = {};
-    char _recv_buff[1024] = {};
+    char headers_buf[MAX_ETH_HEADERS + MAX_IP_TCP_HEADERS + _max_send_size];
+    char send_buff[1024] = {};
+    char recv_buff[1024] = {};
 
 //    typedef struct rx_msg;
 //    rx_msg _msg;
@@ -277,21 +357,23 @@ class Zocket {
 
 int main(int ac, char** av)
 {
-    if (ac != 3) {
-        std::cout << "Usage <InterfaceName> <Host:Port>" << std::endl;
+    if (ac != 5) {
+        std::cout << "Usage <InterfaceName> <Host:Port> <MsgLenInheader> <MsgActualLen>" << std::endl;
         return 0;
     }
     TcpDirect_and_EfVi tcpdirect;
     if (!tcpdirect.init(av[1])) {
         return 0;
     }
-    Zocket zocket(&tcpdirect);
+    int msg_len_in_header = atoi(av[3]);
+    int msg_actual_len = atoi(av[4]);
+    Zocket zocket(&tcpdirect, msg_len_in_header, msg_actual_len);
     if (!zocket.open(av[2])) {
         return 0;
     }
 
     if (!tcpdirect.pio_in_use) {
-
+        zocket.do_write();
     }
 
     zocket.close();
