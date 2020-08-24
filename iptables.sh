30321fa2e7d85e31ff5dc3d8e90a11b0ce01be98
gleb <liftchampion@gmail.com>
2020-08-24 16:37:27 +0300

    minor

--- iptables.sh
+++ iptables.sh
@@ -1,39 +1,35 @@
 
 
-iptables -t nat -A PREROUTING -s 10.3.3.10 -d 10.3.3.20 -j ACCEPT
+iptables -t nat -A PREROUTING -s 10.3.3.10 -d 10.3.3.20 --lenght 1: -j ACCEPT
 
-iptables -t nat -A OUTPUT -s 10.3.3.10 -d 10.3.3.20 -j ACCEPT
+iptables -t nat -A OUTPUT -s 10.3.3.10 -d 10.3.3.20 --lenght 1: -j ACCEPT
 
-iptables -t raw -A PREROUTING -s 10.3.3.10 -d 10.3.3.20 -j ACCEPT
+iptables -t raw -A PREROUTING -s 10.3.3.10 -d 10.3.3.20 --lenght 1: -j ACCEPT
 
-iptables -t mangle -A PREROUTING -s 10.3.3.10 -d 10.3.3.20 -j ACCEPT
+iptables -t mangle -A PREROUTING -s 10.3.3.10 -d 10.3.3.20 --lenght 1: -j ACCEPT
 
-iptables -t nat -A PREROUTING -s 10.3.3.10 -d 10.3.3.20 -j ACCEPT
+iptables -t mangle -A FORWARD -s 10.3.3.10 -d 10.3.3.20 --lenght 1: -j ACCEPT
 
-iptables -t mangle -A FORWARD -s 10.3.3.10 -d 10.3.3.20 -j ACCEPT
+iptables -t filter -A FORWARD -s 10.3.3.10 -d 10.3.3.20 --lenght 1: -j ACCEPT
 
-iptables -t filter -A FORWARD -s 10.3.3.10 -d 10.3.3.20 -j ACCEPT
+iptables -t mangle -A INPUT -s 10.3.3.10 -d 10.3.3.20 --lenght 1: -j ACCEPT
 
-iptables -t mangle -A INPUT -s 10.3.3.10 -d 10.3.3.20 -j ACCEPT
+iptables -t filter -A INPUT -s 10.3.3.10 -d 10.3.3.20 --lenght 1: -j ACCEPT
 
-iptables -t filter -A INPUT -s 10.3.3.10 -d 10.3.3.20 -j ACCEPT
+iptables -t raw -A OUTPUT -s 10.3.3.10 -d 10.3.3.20 --lenght 1: -j ACCEPT
 
-iptables -t raw -A OUTPUT -s 10.3.3.10 -d 10.3.3.20 -j ACCEPT
+iptables -t mangle -A OUTPUT -s 10.3.3.10 -d 10.3.3.20 --lenght 1: -j ACCEPT
 
-iptables -t mangle -A OUTPUT -s 10.3.3.10 -d 10.3.3.20 -j ACCEPT
+iptables -t filter -A OUTPUT -s 10.3.3.10 -d 10.3.3.20 --lenght 1: -j ACCEPT
 
-iptables -t nat -A OUTPUT -s 10.3.3.10 -d 10.3.3.20 -j ACCEPT
+iptables -t mangle -A POSTROUTING -s 10.3.3.10 -d 10.3.3.20 --lenght 1: -j ACCEPT
 
-iptables -t filter -A OUTPUT -s 10.3.3.10 -d 10.3.3.20 -j ACCEPT
+iptables -t nat -A POSTROUTING -s 10.3.3.10 -d 10.3.3.20 --lenght 1: -j ACCEPT
 
-iptables -t mangle -A POSTROUTING -s 10.3.3.10 -d 10.3.3.20 -j ACCEPT
+iptables -t security -A INPUT -s 10.3.3.10 -d 10.3.3.20 --lenght 1: -j ACCEPT
 
-iptables -t nat -A POSTROUTING -s 10.3.3.10 -d 10.3.3.20 -j ACCEPT
+iptables -t security -A FORWARD -s 10.3.3.10 -d 10.3.3.20 --lenght 1: -j ACCEPT
 
-iptables -t security -A INPUT -s 10.3.3.10 -d 10.3.3.20 -j ACCEPT
+iptables -t security -A OUTPUT -s 10.3.3.10 -d 10.3.3.20 --lenght 1: -j ACCEPT
 
-iptables -t security -A FORWARD -s 10.3.3.10 -d 10.3.3.20 -j ACCEPT
-
-iptables -t security -A OUTPUT -s 10.3.3.10 -d 10.3.3.20 -j ACCEPT
-
-iptables -t nat -A INPUT -s 10.3.3.10 -d 10.3.3.20 -j ACCEPT
+iptables -t nat -A INPUT -s 10.3.3.10 -d 10.3.3.20 --lenght 1: -j ACCEPT
